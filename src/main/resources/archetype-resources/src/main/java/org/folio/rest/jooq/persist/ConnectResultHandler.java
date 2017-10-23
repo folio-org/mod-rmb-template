1e22a535d00cf67be03f52f654eb55e246d41683
JD Benito <jd@hybridforge.com>
2017-10-23 13:07:51 -0600

    Deleted the JOOQ-specific helper classes and templates

--- src/main/resources/archetype-resources/src/main/java/org/folio/rest/jooq/persist/ConnectResultHandler.java
+++
@@ -1,8 +0,0 @@
-package org.folio.rest.jooq.persist;
-
-import org.jooq.DSLContext;
-
-public interface ConnectResultHandler {
-  void success(DSLContext db);
-  void failed(Exception exception);
-}
